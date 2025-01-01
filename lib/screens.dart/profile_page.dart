6f953fc40b81a899a0f38ed86dc0e5f8ba4ad191
Veeqy <anoruovictoria1520@gmail.com>
2025-01-01 16:04:45 +0100

    Add existing project files to Git

--- lib/screens.dart/profile_page.dart
+++ lib/screens.dart/profile_page.dart
@@ -19,52 +19,207 @@ class Profile_page extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
-        child:  Padding(
+        child: Padding(
           padding: const EdgeInsets.symmetric(
             vertical: 16,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(
-              height: 30,
-            ),
+            children: [
+              const SizedBox(
+                height: 30,
+              ),
               Row(
                 children: [
                   IconButton(
-                  style: IconButton.styleFrom(
-                    tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                    padding: EdgeInsets.zero,
-                    minimumSize: Size.zero,
-                  ),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => const SoundPage(),
+                    style: IconButton.styleFrom(
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      padding: EdgeInsets.zero,
+                      minimumSize: Size.zero,
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const SoundPage(),
+                        ),
+                      );
+                    },
+                    icon: const Center(
+                      child: CircleAvatar(
+                        backgroundColor: Color.fromARGB(255, 77, 75, 75),
+                        radius: 14,
+                        foregroundColor: Color.fromARGB(255, 248, 246, 246),
+                        child: Icon(Icons.navigate_before_rounded),
                       ),
-                    );
-                  },
-                  icon: const Center(
-                    child: CircleAvatar(
-                      backgroundColor: Color.fromARGB(255, 77, 75, 75),
-                      radius: 14,
-                      foregroundColor: Color.fromARGB(255, 248, 246, 246),
-                      child:Icon(Icons.navigate_before_rounded),
                     ),
                   ),
-                ),
-                const SizedBox(
-                width:80 ,
-                ),
-                const Text('Account profile',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-                ),
+                  const SizedBox(
+                    width: 80,
+                  ),
+                  const Text(
+                    'Account profile',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
                 ],
               ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Name',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                  Text(
+                    'Victoria',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Color.fromARGB(255, 92, 91, 91),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Gender',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                  Text(
+                    'Female',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Color.fromARGB(255, 92, 91, 91),
+              ),
+              const SizedBox(
+                height: 18,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Birthday',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                  Text(
+                    '06-12-2002',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Color.fromARGB(255, 92, 91, 91),
+              ),
+               const SizedBox(
+                height: 18,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      'Email address',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'anoruovictoria1520@gmail.com',
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+              color: Color.fromARGB(255, 92, 91, 91),
+              ),
+               const SizedBox(
+                height: 18,
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Region',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                    ),
+                  ),
+                  Text(
+                    '-',
+                    style: TextStyle(
+                      color: Colors.white70,
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(
+                color: Color.fromARGB(255, 92, 91, 91),
+              ),
+               const SizedBox(
+                height: 18,
+              ),
+              SizedBox(
+              height: 53,
+              child: OutlinedButton(
+                onPressed: () {
+                  debugPrint('Received click');
+                },
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(
+                    width: 1.5,
+                    color: Color(0xFF82E600),
+                    style: BorderStyle.solid,
+                  ),
+                ),
+                child: const Text(
+                  'Log in',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Color(0xFF82E600),
+                  ),
+                ),
+              ),
+            )
             ],
           ),
         ),
